beb36b112c0905ed89564efb01f3bfade3fdb81c
englisphere <englisphereppg@gmail.com>
2025-05-02 01:49:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ document.addEventListener("DOMContentLoaded", function () {
       const errorMsg = document.getElementById("loginError");
 
       if (username === "rizky" && password === "rizky123") {
-        window.location.href = "dasbor-rizky.html";
+        window.location.href = "https://englisphere.github.io/murid/dasbor-rizky.html";
       } else if (username === "andy" && password === "andy123") {
         window.location.href = "dasbor-andy.html";
       } else {
