b3722560a17c049f1548d474e08218dbb92ec6f3
englisphere <englisphereppg@gmail.com>
2025-05-02 00:55:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -42,3 +42,42 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("loginForm");
+  if (form) {
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
+
+      const username = document.getElementById("username").value.trim();
+      const password = document.getElementById("password").value.trim();
+      const errorMsg = document.getElementById("loginError");
+
+      if (username === "rizky" && password === "rizky123") {
+        window.location.href = "dasbor-rizky.html";
+      } else if (username === "andy" && password === "andy123") {
+        window.location.href = "dasbor-andy.html";
+      } else {
+        errorMsg.textContent = "Username atau password salah.";
+      }
+    });
+  }
+});
